b0d0441c8ac456b43a0683269a70a22471b69d5b
aayushker <singhaayushker@gmail.com>
2025-01-08 13:15:02 +0530

    refactor(NavBar): update layout and comment out Clerk components

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/app/components/ui/button";
 import { Link } from "@nextui-org/react";
+// import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 
 export default function NavBar() {
   return (
@@ -8,14 +9,19 @@ export default function NavBar() {
         <Link href="/" className="text-2xl font-bold text-blue-900">
           Devfoolio
         </Link>
-        <div className="space-x-4">
+        <div className="space-x-4 flex items-center">
           <Link href="/" className="text-blue-900 hover:text-blue-700">
             About
           </Link>
           <Link href="/" className="text-blue-900 hover:text-blue-700">
             Contact
           </Link>
-          <Button variant="outline">Log In</Button>
+          {/* <SignedOut> */}
+            <Button variant="outline">Log In</Button>
+          {/* </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn> */}
         </div>
       </nav>
     </header>
