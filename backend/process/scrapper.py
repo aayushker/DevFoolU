7e75ac96da66614380ee5a15bca8cc057a311068
aayushker <singhaayushker@gmail.com>
2025-04-04 23:49:34 +0530

    feat: Configure Chrome WebDriver to run in headless mode for scrapper

--- backend/process/scrapper.py
+++ backend/process/scrapper.py
@@ -2,9 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 def scrapper(projectURL):
-    driver = webdriver.Chrome()
+    # Configure Chrome to run in headless mode
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+    
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get(projectURL)
 
     project_name = ""
