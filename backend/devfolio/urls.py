9b74c0fbb96f91d63c74aa02fd9f90ccc7de8fd0
aayushker <singhaayushker@gmail.com>
2025-04-06 23:55:09 +0530

    feat: Add new API endpoints for heartbeat and stats in Devfolio URLs

--- backend/devfolio/urls.py
+++ backend/devfolio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Devfolio
+from .views import Devfolio, Heartbeat, Stats
 
 urlpatterns = [
     path('project/', Devfolio.as_view(), name='devfolio'),
+    path('heartbeat/', Heartbeat, name='api_heartbeat'),
+    path('stats/', Stats, name='api_stats'),
 ]
